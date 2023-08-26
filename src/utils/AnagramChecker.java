7e1df90ee29ec7fea526cb78f2d44dda331021b6
Shamil <gutshellbreath@gmail.com>
2023-08-26 21:12:26 +0300

    added anagram checker

--- src/utils/AnagramChecker.java
+++ src/utils/AnagramChecker.java
@@ -1,2 +1,24 @@
-package utils;public class AnagramChecker {
+package utils;
+
+import java.util.Arrays;
+
+public class AnagramChecker {
+
+    private char[] sArray;
+    private char[] tArray;
+
+    public void isAnagram(String s, String t) {
+        sArray = s.toCharArray();
+        tArray = t.toCharArray();
+
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        getResult(s, t, Arrays.equals(sArray, tArray));
+    }
+
+    private void getResult(String s, String t, boolean result) {
+        System.out.println("\n" + "Input: s = \"" + s + "\", t = \"" + t + "\"\nOutput: " + result);
+    }
+
 }
